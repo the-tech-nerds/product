7a4e4abd7c6f6fe39aca0c3608169f688c84264e
arifulcuet52@gmail.com <arifulcuet52@gmail.com>
2021-03-20 23:31:52 +0600

    bug fixed

--- src/products/services/product/create-product.service.ts
+++ src/products/services/product/create-product.service.ts
@@ -4,6 +4,7 @@ import { Product } from 'src/products/entities/product.entity';
 import { ProductRequest } from 'src/products/requests/product.request';
 import { FetchCategoryByIdService } from 'src/categories/service/fetch-category-by-id.service';
 import { Repository } from 'typeorm';
+import { Category } from 'src/categories/entities/category.entity';
 
 @Injectable()
 class CreateProductService {
@@ -18,22 +19,22 @@ class CreateProductService {
     userId: number,
     productRequest: ProductRequest,
   ): Promise<Product> {
-    const product = await this.productRepository.save({
-      ...productRequest,
-      created_by: userId,
-    });
-
+    let category: Category | null = null;
     if (productRequest.category_id) {
       // @ts-ignore
-      const category = await this.fetchCategoryByIdService.execute(
+      category = await this.fetchCategoryByIdService.execute(
         productRequest.category_id,
       );
       if (!category) {
         throw new BadRequestException('Not a valid category');
       }
-      product.categories.push(category);
-      return this.productRepository.save(product);
     }
+    const product = await this.productRepository.save({
+      ...productRequest,
+      created_by: userId,
+      categories: category ? [category] : undefined,
+    });
+
     return product;
   }
 }
